94d94fc8e2798c3e06d47ec92c5a8c88f13d5bd7
GILL71 <gm360@mail.ru>
2018-02-05 11:57:08 +0300

    add: animation for loading vc

--- SmashingFlashing/Controllers/LoadViewController.swift
+++ SmashingFlashing/Controllers/LoadViewController.swift
@@ -10,60 +10,56 @@ import UIKit
 
 class LoadViewController: UIViewController {
 
-    @IBOutlet weak var startImage: UIImageView!
-    @IBOutlet weak var rotateImage: UIImageView!
+    @IBOutlet weak var transitionView: UIImageView!
+    @IBOutlet weak var rotatingView: UIImageView!
+    
+    let width = 180
+    let heightMain = 140
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //startImage.image = UIImage(contentsOfFile: "start.png")
-        //rotateImage.image = UIImage(contentsOfFile: "chelk.png")
+        rotatingView.layer.anchorPoint = CGPoint(x: 0.1, y: 0.5)
+        
+        transitionView.frame = CGRect(x: 0, y: 0, width: width, height: heightMain)
+        transitionView.center = self.view.center
+        
+        rotatingView.frame = CGRect(x: 0, y: 0, width: width, height: heightMain / 7)
+        rotatingView.center = CGPoint(x: self.view.center.x - CGFloat(width/2 - 18),y: transitionView.center.y + rotatingView.frame.height / 2 - transitionView.frame.height / 2)
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        startAnimation()
+        rightRotation()
     }
     
-    func startAnimation() {
-        setAnchorPoint(anchorPoint: CGPoint(x: 0.0,y: 1.0), view: rotateImage)
-        //rotateImage.layer.anchorPoint = CGPoint(x: 1.0, y: 1.0)
-        //rotateImage.layer.position = CGPoint(x: 150.0, y: 300.0)
-        UIView.animate(withDuration: 4.5) {
-            self.rotateImage.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
-            //self.rotateImage.transform = CGAffineTransform(
-        }
-        delay(10.00) {
-            self.changeRootViewController()
+    func rightRotation() {
+        UIView.animate(withDuration: 1, animations: {
+            self.rotatingView.transform = CGAffineTransform.init(rotationAngle: -CGFloat.pi / 4)
+        }) { yes in
+            if yes {
+                self.leftRotation()
+            }
         }
     }
     
-    func setAnchorPoint(anchorPoint: CGPoint, view: UIView) {
-        var newPoint = CGPoint(x:view.bounds.size.width * anchorPoint.x, y:view.bounds.size.height * anchorPoint.y)
-        var oldPoint = CGPoint(x:view.bounds.size.width * view.layer.anchorPoint.x, y:view.bounds.size.height * view.layer.anchorPoint.y)
-        
-        newPoint = newPoint.applying(view.transform)
-        oldPoint = oldPoint.applying(view.transform)
-        
-        var position : CGPoint = view.layer.position
-        
-        position.x -= oldPoint.x
-        position.x += newPoint.x;
-        
-        position.y -= oldPoint.y;
-        position.y += newPoint.y;
-        
-        view.layer.position = position;
-        view.layer.anchorPoint = anchorPoint;
+    func leftRotation() {
+        UIView.animate(withDuration: 0.2, animations: {
+            self.rotatingView.transform = CGAffineTransform.init(rotationAngle: 0)
+        }) { yes in
+            if yes {
+                self.leftOutTransform()
+            }
+        }
     }
     
-    func changeRootViewController() {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let nav =  storyboard.instantiateViewController(withIdentifier: "TabBarController") as! UITabBarController
-        self.present(nav, animated: true, completion: nil)
-    }
-    
-    public func delay(_ delay: Double, closure: @escaping ()->()) {
-        DispatchQueue.main.asyncAfter(
-            deadline: DispatchTime.now() + Double(Int64(delay * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC), execute: closure)
+    func leftOutTransform() {
+        UIView.animate(withDuration: 0.4, animations: {
+            self.rotatingView.transform = CGAffineTransform.init(translationX: -300, y: self.rotatingView.layer.bounds.minY)
+            self.transitionView.transform = CGAffineTransform.init(translationX: -300, y: self.transitionView.layer.bounds.minY)
+        }) { yes in
+            if yes {
+                self.performSegue(withIdentifier: "startVc", sender: self)
+            }
+        }
     }
 }
